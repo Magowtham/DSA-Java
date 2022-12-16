0357eb7180f55d8cdaa7d20cbf054230783a5c94
Gowtham <magowtham7@gmail.com>
2022-12-16 19:34:57 +0530

    java added

---
+++ LinearsearchM.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class LinearsearchM{
+    public static void main(String[] args) {
+      Scanner sc=new Scanner(System.in);
+      int a[][]={
+        {1,2,3},
+        {2,1,4},
+        {3,34,54,3}
+      };
+      int key=54;
+      if(searchl(a,key)==true)
+      {
+        System.out.println("item found");
+      }
+      else{
+        System.out.println("item not found");
+      }
+    
+
+    }
+    public static boolean searchl(int a[][],int key)
+    {
+     for(int b[]:a)
+     {
+       for(int x:b)
+       {
+        if(x==key)
+        {
+            return true;
+        }
+       }
+     }
+     return false;
+    }
+
+}
