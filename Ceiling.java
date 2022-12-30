1e2af6a3e43691f01af83ea627f2ab63f5f58905
Gowtham <magowtham7@gmail.com>
2022-12-30 20:05:50 +0530

    modified file

---
+++ Ceiling.java
@@ -0,0 +1,31 @@
+public class Ceiling {
+   public static void main(String[] args) {
+    int arr[]={2,3,5,9,14,16};
+    int target=16;
+    System.out.println(bigelementsearch(arr, target));
+   
+   } 
+   static int bigelementsearch(int arr[],int target)
+   {
+    int start=0;
+    int end=arr.length-1;
+    int big=target;
+    while(start<=end)
+    {
+        int mid=start+(end-start)/2;
+        if(target==arr[mid])
+        {
+        return arr[mid];
+        }
+        else if(arr[mid]>big)
+        {
+          big=arr[mid];
+          end=mid-1;
+        }
+        else{
+            start=mid+1;
+        }
+    }
+    return (big==target)?-1:big;
+   }
+}
